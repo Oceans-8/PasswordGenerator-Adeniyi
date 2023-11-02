039da1cd8966d0b9e163cdbb12538fd5de26348f
Oceans-8 <147375255+Oceans-8@users.noreply.github.com>
2023-11-02 23:27:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,7 @@
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
-
-//length, if outside 8-128, then return error
-//4 separate questions
-//- lowercase, uppercase, numeric, and/or special characters
-
-// note when user writes a number in prompt it is treated as string not number so it has to be converted
-
-// use The concat() method of Array instances is used to merge two or more arrays
-// This method does not change the existing arrays, but instead returns a new array
-
-//const aNumber = Number(window.prompt("Type a number", ""));
-
-// window.prompt ("Would you like to include lower case letters? (okay for yes, cancel for no)");
-//window.prompt ("Would you like to include upper case letters? (okay for yes, cancel for no)");
-//window.prompt ("Would you like to include numeric digits? (okay for yes, cancel for no)");
-//window.prompt ("Would you like to include symbols? (okay for yes, cancel for no)");
-//return 'password';
-
-//for loop to 128
-//window.confirm
-
-// next steps
-// add loops
-// check numbers
-// empty initial
-// letter inputs
-
-
 var digits=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var uppercase=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var lowercase=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var symbols=['!', '@', '#', '$', '%', '^', '&', '*', '?']
-
+var symbols=['!', '@', '#', '$', '%', '^', '&', '*', '?', '(', ')', '+', '-', ',', '.', '/', ':', ';', '<', '=', '>', '[', ']', '{', '}', '~', '`', '|']
 
 var generatePassword = function () {
   var LengthQ = parseInt(window.prompt ("How many characters would you like in your password?"));
@@ -97,7 +56,7 @@ var generatePassword = function () {
 var generateBtn = document.querySelector("#generate");
 
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -106,5 +65,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
+
 generateBtn.addEventListener("click", writePassword);
